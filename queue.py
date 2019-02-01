e042d199e3c5e087948af2a1a93dac24e0c5d45a
blues <470960070@qq.com>
2019-02-01 13:56:59 +0800

    new

---
+++ queue.py
@@ -0,0 +1,52 @@
+class Queue(object):
+
+    def __init__(self):
+        self.queue = []
+
+    def in_queue(self,*args):
+        self.queue.extend(args)
+
+    def out_queue(self):
+        if not self.queue == []:
+            self.queue.pop(0)
+        else:
+            return None
+
+    def show(self):
+        for item in self.queue:
+            print(item)
+
+    def head(self):
+        if not self.queue == []:
+            return self.queue[0]
+        else:
+            return None
+
+    def tail(self):
+        if not self.queue == []:
+            return self.queue[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return self.queue == []
+
+    def length(self):
+        return len(self.queue)
+
+if __name__ == '__main__':
+    q =Queue()
+    q.in_queue(1)
+    q.show()
+
+    q.in_queue(2,3,4,5)
+    q.show()
+
+    q.out_queue()
+    q.show()
+
+    print(q.head())
+    print(q.tail())
+
+    print(q.length())
+    print(q.is_empty())
---
+++ stack.py
@@ -0,0 +1,35 @@
+class Stack(object):
+    __slots__ = ('__items',)
+
+    def __init__(self):
+        self.__items = []
+
+    def is_empty(self):
+        return self.__items == []
+
+    def size(self):
+        return len(self.__items)
+
+    def peek(self):
+        return self.__items[self.size() - 1]
+
+    def push(self,item):
+        self.__items.append(item)
+
+    def pop(self):
+        return self.__items.pop()
+
+if __name__ == '__main__':
+    s = Stack()
+    s.push('h')
+    s.push('a')
+    print(s.size())
+    print(s.peek())
+    print(s.pop())
+    print(s.peek())
+
+    print(s.size())
+    print(s.pop())
+    print(s.size())
+
+    print(s.is_empty())
